0669929297b36eabf2589fc4a0d717950571c4fd
Ziaurrehman Malik <112377951+Ziaurrehman90@users.noreply.github.com>
2024-02-22 23:57:23 +0530

    Add files via upload

---
+++ Cat.js
@@ -0,0 +1,11 @@
+function Cat(){
+    return(
+    <div>
+        {/* we can alsoa dd the styong part to the tag  */}
+        <h1 style={{fontSize:"40px",color:"orange",fontWeight:"bold",fontFamily:"cursive"}}>
+            Hi, I'm meau!!
+        </h1>
+    </div>
+    );
+}
+export default Cat;
